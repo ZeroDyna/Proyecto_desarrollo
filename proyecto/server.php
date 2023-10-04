5cdc460af33f78a41480ef549be13ae8aa660314
ZeroDyna <93954219+ZeroDyna@users.noreply.github.com>
2023-10-04 14:11:06 -0500

    actualizacion de prieba y php

--- proyecto/server.php
+++ proyecto/server.php
@@ -1,21 +1,57 @@
 <?php 
-$link = mysql_connect("localhost","root","") or die("<h2>No se encuentra el servidor</h2>");;
-$db = mysql_select_db("usuarios", $link) or die("<h2>Error de conexion</h2>");
+$user = "root";
+$pass = "";
+$host = "localhost";
+
+$connection = mysqli_connect($host, $user, $pass);
 
 $nombre = $_POST[""];
 $email = $_POST["email"];
 $password = $_POST["psw"];
 $rPass = $_POST["psw-repeat"];
 
-$req = (strlen($nombre) * strlen($email) * strlen($password)*strlen($rPass)) or die("No se han llenado todos los casilleros");
-
-if ($password != $rPass){
-    die("Las contrasenas no coinciden <br><br>");
+if ($password!=$rPass){
+    die('Las contrasenas no coinciden <br><br><a href="prieba.html"> Volver</a>');
 }
 
-$contrasenaUsuario = md5($password);
+if(!$connection) 
+        {
+            echo "No se ha podido conectar con el servidor" ;
+        }
+  else
+        {
+            echo "<b><h3>Hemos conectado al servidor</h3></b>" ;
+        }
+        //indicamos el nombre de la base datos
+        $datab = "pag_web_libros";
+        //indicamos selecionar ala base datos
+        $db = mysqli_select_db($connection,$datab);
 
-mysql_query("INSERT INTO pag_web_libros VALUES ('','$nombre','$email','$contrasenaUsuario')",$link) or die("Error de envio");
+        if (!$db)
+        {
+        echo "No se ha podido encontrar la Tabla";
+        }
+        else
+        {
+        echo "<h3>Tabla seleccionada:</h3>" ;
+        }
+        //insertamos datos de registro al mysql xamp, indicando nombre de la tabla y sus atributos
+        $instruccion_SQL = "INSERT INTO usuario (nombre, correo, contrasena)
+                             VALUES ('$nombre','$email','$password')";
+                           
+                            
+        $resultado = mysqli_query($connection,$instruccion_SQL);
 
+        //$consulta = "SELECT * FROM tabla where id ='2'"; si queremos que nos muestre solo un registro en especifivo de ID
+        $consulta = "SELECT * FROM usuario";
+        
+$result = mysqli_query($connection,$consulta);
+if(!$result) 
+{
+    echo "No se ha podido realizar la consulta";
+}
+
+
+mysqli_close( $connection );
 
 ?>
